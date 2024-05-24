1f0d6fab0f2a4cd99208172e22170c39acb10cb1
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-05-24 10:58:02 +0200

    loader stilizzato

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -365,7 +365,9 @@ export default{
 <!--fine container-->
 
 <!-- Loader -->
-<div v-if="loading" class="loader">Loading...</div>
+<div v-if="loading" class="loader-box">
+    <span class="loader"></span>
+</div>
 
 </template>
 
@@ -401,18 +403,51 @@ i{
     }
 }
 
+/* loader */
+.loader-box {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.826);
+    z-index: 9999;
+}
 .loader {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-  font-size: 1.5rem;
-  color: #000;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%);
+    width:70px;
+    height: 180px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.loader:after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    padding: 3px 6px;
+    border-top: 1px solid #bbb6aa;
+    border-bottom: 2px solid #bbb6aa;
+    background: linear-gradient(#eb6b3e 0 0) bottom no-repeat content-box,#e4e0d7;
+    mix-blend-mode: darken;
+    animation: l1 1.7s infinite linear;
+}
+
+.loader:before {
+    content: "";
+    position: absolute;
+    inset: -30px calc(50% - 2px) 6px;
+    background: #612329;
+    transform-origin: bottom;
+    transform: rotate(8deg);
+}
+
+@keyframes l1 {
+    0%   {background-size: 100% 100%}
+    100% {background-size: 100% 0%}
 }
 </style>
